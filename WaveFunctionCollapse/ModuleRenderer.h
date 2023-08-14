d6adadd2bd7265bf81c034043acbf88b1bbe931c
DavidTello1 <davidtellop@gmail.com>
2023-08-14 18:25:18 +0200

    Blit function

--- WaveFunctionCollapse/ModuleRenderer.h
+++ WaveFunctionCollapse/ModuleRenderer.h
@@ -2,6 +2,7 @@
 #include "Module.h"
 
 #include "SDL2/include/SDL.h"
+#include <limits.h>
 
 class ModuleRenderer : public Module
 {
@@ -18,6 +19,7 @@ public:
 	SDL_Renderer* GetRenderer() const { return renderer; }
 
 	bool DrawQuad(const SDL_Rect& rect, Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255, bool filled = true, bool use_camera = true) const;
+	bool Blit(SDL_Texture* texture, int x, int y, const SDL_Rect* section = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE, float speed = 1.0f, double angle = 0, int pivot_x = INT_MAX, int pivot_y = INT_MAX) const;
 
 private:
 	SDL_Renderer* renderer = nullptr;
