3c683b24198fe74e0039542e63af053c3d6cd0a8
DavidTello1 <davidtellop@gmail.com>
2023-08-20 17:44:57 +0200

    Default shader

--- Game/Assets/Shaders/shaders.glsl
+++ Game/Assets/Shaders/shaders.glsl
@@ -39,32 +39,6 @@ void main()
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
-#ifdef GRID_SHADER
-
-#if defined(VERTEX) ///////////////////////////////////////////////////
-
-layout(location = 0) in vec3 aPosition;
-
-uniform mat4 uViewProj;
-
-void main()
-{
-	gl_Position = uViewProj * vec4(aPosition, 1.0);
-}
-
-#elif defined(FRAGMENT) ///////////////////////////////////////////////
-
-uniform vec4 uColor;
-out vec4 oColor;
-
-void main()
-{
-	oColor = uColor;
-}
-
-#endif
-#endif
-
 
 // NOTE: You can write several shaders in the same file if you want as
 // long as you embrace them within an #ifdef block (as you can see above).
