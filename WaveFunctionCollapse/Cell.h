b12d2297bbdf1bc1bcd180e48665a6ef6810026f
DavidTello1 <davidtellop@gmail.com>
2023-08-23 19:27:28 +0200

    Cell Reset & Set

--- WaveFunctionCollapse/Cell.h
+++ WaveFunctionCollapse/Cell.h
@@ -4,24 +4,25 @@
 struct Cell 
 {
 public:
+	// --- Constructor
 	Cell(int index, unsigned int maskSize) {
 		tileID = -1;
 		isCollapsed = false;
+		isInvalid = false;
+		isPreset = false;
 
 		this->index = index;
 		this->mask = new BitMask(maskSize);
 		this->mask->SetAll();
 	};
 
+	// --- Destructor
 	~Cell() {
 		delete mask;
 		mask = nullptr;
 	}
 
-	int GetEntropy() const {
-		return mask->Count();
-	}
-
+	// --- Main Functions
 	void Update() {
 		if (mask->Count() == 1)
 			Observe();
@@ -29,17 +30,41 @@ public:
 
 	void Observe() {
 		tileID = mask->Pick();
+		isCollapsed = true;
 
-		if (tileID != -1)
-			isCollapsed = true;
-		else
+		if (tileID == -1)
 			isInvalid = true;
+	}
+
+	// --- Utils
+	int GetEntropy() const {
+		return mask->Count();
+	}
+
+	void Reset() {
+		tileID = -1;
+		isCollapsed = false;
+		isInvalid = false;
+		isPreset = false;
+		mask->SetAll();
+	}
+	
+	void SetCell(int tileID) {
+		//if (tileID < 0 || tileID > maskSize) //***MASK SIZE
+		//	return;
+
+		this->tileID = tileID;
+		isCollapsed = true;
+		isInvalid = false;
+		mask->ClearAll();
+		mask->SetBit(tileID, true);
 	}
 
 public:
 	int index = -1;
 	int tileID = -1;
-	BitMask* mask;
 	bool isCollapsed = false;
 	bool isInvalid = false;
+	bool isPreset = false;
+	BitMask* mask;
 };
