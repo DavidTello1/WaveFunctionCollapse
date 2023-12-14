cf0142ab2a4a96fb1f15b092bdd6f31a15d14adf
DavidTello1 <davidtellop@gmail.com>
2023-12-14 01:23:47 +0100

    Tileset class

--- Source/Tile.h
+++ Source/Tile.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "StaticArray.h"
+#include "DynArray.h"
 #include "BitArray.h"
 
 #define NUM_NEIGHBOURS 4
 
 struct Tile 
 {
+	friend class Tileset;
+
 public:
 	Tile(const int id, unsigned int texture, const StaticArray<BitArray, NUM_NEIGHBOURS> masks) : id(id), texture(texture), masks(masks) {};
 
@@ -23,8 +26,66 @@ public:
 	unsigned int GetTexture() const { return texture; }
 	StaticArray<BitArray, NUM_NEIGHBOURS> GetMasks() const { return masks; }
 
-private:
+protected:
 	int id = -1;
 	unsigned int texture = 0;
 	StaticArray<BitArray, NUM_NEIGHBOURS> masks; // top, left, right, bottom
+};
+
+// -------------------------------
+class Tileset
+{
+private:
+	DynArray<Tile*> tiles;
+
+
+public:
+	Tileset() 
+	{
+	}
+
+	~Tileset() 
+	{
+		for (unsigned int i = 0; i < tiles.size(); ++i)
+			delete tiles[i];
+		tiles.clear();
+	}
+
+	Tile* GetTile(int index)
+	{
+		return tiles[index];
+	}
+
+	const Tile* GetTile(int index) const 
+	{ 
+		return tiles[index];
+	}
+
+	const unsigned int GetSize() const 
+	{
+		return tiles.size(); 
+	}
+
+	void AddTile(Tile* tile)
+	{
+		tiles.push_back(tile);
+	}
+
+	void RemoveTile(int index)
+	{
+		//tiles.erase(index);
+	}
+
+	void UpdateTile(int index, const Tile& tile)
+	{
+		*tiles[index] = tile;
+	}
+
+	void UpdateMask(int index, int dir, int bit, bool value)
+	{
+		if (value)
+			tiles[index]->masks[dir].setBit(bit);
+		else
+			tiles[index]->masks[dir].clearBit(bit);
+	}
 };
